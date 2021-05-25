dd7a85120c0baaecbcb9d95677bbfbff4a91e85a
bensarr <bensarr37@gmail.com>
2021-05-25 20:30:15 +0000

    Created project

---
+++ src/test/java/dev/benswift/cours/CoursApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.benswift.cours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
